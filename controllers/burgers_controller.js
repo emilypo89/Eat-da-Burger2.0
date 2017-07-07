294900568fab6c57c2d7ea3a5dac9e4aa4205e02
emilypo89 <emilypo89@gmail.com>
2017-07-06 22:43:30 -0400

    renders page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,3 @@
-
 // Dependencies
 var express = require("express");
 var db = require("../models");
@@ -7,39 +6,38 @@ console.log(db.Burger);
 var router = express.Router();
 
 
-module.exports = function(app) {
-	// get request to show the index.handlebars on the page 
-	// shows all of the burgers currently in the database on the page
-	router.get("/", function(req, res) {
-		console.log(req);
-		db.Burger.findAll({}).then(function(data) {
-			console.log(data)
-			// var burgerObject = ;	
-	     res.render("index", {burgerObject: data});
-	    });
-	});
 
-	// post request to add a new burger to the list of burgers
-	// redirected back the the get request to show all the burgers, including the new one on the page
-	router.post("/", function(req, res) {
-	    Burger.create({
-	      burger_name: req.body.name,
-	      devoured: req.body.devoured
-	    }).then(function(data){
-	      res.redirect("/");
-	    });
-	});
+    // get request to show the index.handlebars on the page 
+    // shows all of the burgers currently in the database on the page
+    router.get("/", function(req, res) {
+        db.Burger.findAll({}).then(function(data) {
+            console.log(data)    
+         res.render("index", {burgers: data});
+        });
+    });
 
-	// put request to update the page when the devoured state changes from false to true
-	router.put("/:id", function(req, res) {
-		Burger.update({
-		  devoured: req.body.devoured,
-		}, {
-		  where: {
-		    id: req.body.id
-		  }
-		}).then(function(data) {
-			res.redirect("/");
-		});
-	});
-}
+    // post request to add a new burger to the list of burgers
+    // redirected back the the get request to show all the burgers, including the new one on the page
+    router.post("/", function(req, res) {
+        db.Burger.create({
+          burger_name: req.body.name,
+          devoured: req.body.devoured
+        }).then(function(data){
+          res.redirect("/");
+        });
+    });
+
+    // put request to update the page when the devoured state changes from false to true
+    router.put("/:id", function(req, res) {
+        db.Burger.update({
+          devoured: req.body.devoured,
+        }, {
+          where: {
+            id: req.body.id
+          }
+        }).then(function(data) {
+            res.redirect("/");
+        });
+    });
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,15 @@
-// burger model
 module.exports = function(sequelize, DataTypes) {
-  var Burger = sequelize.define("burgers", {
+  var Burger = sequelize.define("Burger", {
     burger_name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 140]
+        len: [1]
       }
     },
     devoured: {
       type: DataTypes.BOOLEAN,
+      allowNull: false,
       defaultValue: false
     }
   });
